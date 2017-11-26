de58fddd82f4136cee9c4b0f0caacd16776e625f
Andrey Lisovskiy <andrlis0506@gmail.com>
2017-11-26 13:31:24 +0300

    Get list of usb-devices

---
+++ bean/Device.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsbEject;
+
+namespace USB_Manager.bean
+{
+    class Device
+    {
+        private string name;
+        private char tomeName;
+        private long totalMemory;
+        private long usedMemory;
+        private long freeMemory;
+        private DirectoryInfo root;
+        private bool extracted;
+
+        public Device() { }
+
+        public Device(string name, char tomName, long totalMemory, 
+            long usedMemory, long freeMemory, DirectoryInfo root) {
+            this.name = name;
+            this.tomeName = tomName;
+            this.totalMemory = totalMemory;
+            this.freeMemory = freeMemory;
+            this.usedMemory = usedMemory;
+            this.root = root;
+        }
+        
+        public string Name
+        {
+            get { return name; }
+            set { this.name = value; }
+        }
+
+        public char TomeName
+        {
+            get { return tomeName; }
+            set { this.tomeName = value; }
+        }
+
+        public long TotalMemory
+        {
+            get { return totalMemory; }
+            set { this.totalMemory = value; }
+        }
+
+        public long FreeMemory
+        {
+            get { return freeMemory; }
+            set { this.freeMemory  = value; }
+        }
+
+        public long UsedMemory
+        {
+            get { return this.usedMemory; }
+            set { this.usedMemory = value; }
+        }
+
+        public DirectoryInfo Root
+        {
+            get { return this.root; }
+            set { this.root = value; }
+        }
+
+        public bool Extracted
+        {
+            get { return this.extracted; }
+            set { this.extracted = value; }
+        }
+
+        public bool Eject()
+        {
+            if (!extracted)
+            {
+                return false;
+            }
+
+            VolumeDeviceClass volumeDeviceClass = new VolumeDeviceClass();
+            foreach (Volume dev in volumeDeviceClass.Devices)
+            {
+                try
+                {
+                    if (dev.LogicalDrive.Equals(this.tomeName + ":"))
+                    {
+                        dev.Eject(true);
+                        volumeDeviceClass = new VolumeDeviceClass();
+                        foreach (Volume temp in volumeDeviceClass)
+                        {
+                            if (temp.LogicalDrive.Equals(this.tomeName + ":"))
+                                return false;
+                        }
+                        return true;
+                    }
+                }
+                catch
+                {
+                    break;
+                }
+            }
+
+            return false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var device = obj as Device;
+            return device != null &&
+                   name == device.name &&
+                   tomeName == device.tomeName &&
+                   totalMemory == device.totalMemory &&
+                   usedMemory == device.usedMemory &&
+                   freeMemory == device.freeMemory &&
+                   EqualityComparer<DirectoryInfo>.Default.Equals(root, device.root) &&
+                   extracted == device.extracted;
+        }
+    }
+}
---
+++ resources/UsbManager.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using MediaDevices;
+using WpdMtpLib;
+using USB_Manager.bean;
+
+namespace USB_Manager.resources
+{
+    class UsbManager
+    {
+        public static List<Device> GetDeviceList()
+        {
+            List<Device> answer = new List<Device>();
+            foreach (var currentMediaDevice in MediaDevice.GetDevices())
+            {
+                Device device = new Device();
+                try
+                {
+                    currentMediaDevice.Connect();
+                }
+                catch (System.IO.DirectoryNotFoundException ex)
+                {
+                    continue;
+                }
+
+                device.Name = currentMediaDevice.FriendlyName;
+
+                if (currentMediaDevice.DeviceType == MediaDevices.DeviceType.Generic)       //If drive name is like a current device name.
+                {
+                    UsbManager.SetUsbDrive(device);
+                }
+                else
+                {
+                    device.Extracted = false;
+                    device.Name += " - " + currentMediaDevice.Model;
+
+                    UsbManager.SetMtpDevice(device, currentMediaDevice);
+                }
+                currentMediaDevice.Disconnect();
+                answer.Add(device);
+            }
+         return answer;
+        }
+
+        private static void SetUsbDrive(Device device)
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();                          //Get names of all logical drives.
+            foreach (var currentDriver in allDrives)
+            {
+                if (currentDriver.DriveType == DriveType.Removable)                 //If drive is removable.
+                {
+                    if (currentDriver.VolumeLabel == device.Name)
+                    {
+                        device.TomeName = currentDriver.Name[0];
+                        device.Root = currentDriver.RootDirectory;
+                        device.TotalMemory = currentDriver.TotalSize;
+                        device.FreeMemory = currentDriver.TotalFreeSpace;
+                        device.UsedMemory = device.TotalMemory - device.FreeMemory;
+                        device.Extracted = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static void SetMtpDevice(Device device, MediaDevice currentMediaDevice)
+        {
+            MtpResponse mtpResponse;
+            MtpCommand mtpCommand = new MtpCommand();
+
+            string[] deviceIds = mtpCommand.GetDeviceIds();
+            if (deviceIds.Length == 0) { return; }
+
+            string targetDeviceId = String.Empty;
+
+            foreach (string deviceId in deviceIds)
+            {
+                if (currentMediaDevice.FriendlyName.Equals(mtpCommand.GetDeviceFriendlyName(deviceId)))
+                {
+                    targetDeviceId = deviceId;
+                    break;
+                }
+            }
+
+            if (targetDeviceId.Length == 0) { return; }
+            mtpCommand.Open(targetDeviceId);
+
+            uint[] storageIds = null;
+
+            mtpResponse = mtpCommand.Execute(MtpOperationCode.GetStorageIDs, null, null);
+
+            if (mtpResponse != null)
+            {
+                storageIds = Utils.GetUIntArray(mtpResponse.Data);
+
+                if (storageIds.Length != 0)
+                {
+                    try
+                    {
+                        mtpResponse = mtpCommand.Execute(MtpOperationCode.GetStorageInfo, new uint[1] { storageIds[0] }, null);
+                        StorageInfo storageInfo = new StorageInfo(mtpResponse.Data);
+
+                        device.TotalMemory = (long)storageInfo.MaxCapacity;
+                        device.FreeMemory = (long)storageInfo.FreeSpaceInBytes;
+                        device.UsedMemory = device.TotalMemory - device.FreeMemory;
+                    }
+                    catch
+                    {
+                        device.TotalMemory = device.FreeMemory = device.UsedMemory = 0;
+                    }
+                }
+            }
+        }
+    }
+}
